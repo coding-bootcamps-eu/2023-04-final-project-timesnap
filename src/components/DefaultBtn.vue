929dcde8ffe95e38f048a07262dae30d7faba6be
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-05 23:42:48 +0200

    feat: adds Default Button component

---
+++ src/components/DefaultBtn.vue
@@ -0,0 +1,33 @@
+<template>
+  <button class="btn">{{ btnText }}</button>
+</template>
+<script>
+export default {
+  props: {
+    btnText: {
+      type: String,
+      required: true,
+      default: "click here",
+    },
+  },
+};
+</script>
+
+<style scoped>
+.btn {
+  border-radius: 2rem;
+  padding: 0.75rem;
+  border: 1px solid #d6dde6;
+  background: none;
+  cursor: pointer;
+}
+
+.btn--Primary {
+  background: #2c3c54;
+  color: #ffffff;
+}
+
+.btn--Secondary {
+  background: #e2e6ee;
+}
+</style>
