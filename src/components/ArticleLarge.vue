b745d3525dea85caa770aa75213db066783b6ad2
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-07 00:22:15 +0200

    style: adds styling to img, line height und font color

--- src/components/ArticleLarge.vue
+++ src/components/ArticleLarge.vue
@@ -1,15 +1,56 @@
 <!-- Contains title, text block, image and if necessary a button -->
 
 <template>
-  <div></div>
+  <article>
+    <section>
+      <h2 class>{{ title }}</h2>
+      <slot />
+      <DefaultBtn v-if="btnText !== 'See more'" :btnText="btnText" />
+    </section>
+    <img v-if="imgSrc !== undefined" :src="imgSrc" alt="" />
+  </article>
 </template>
-
 <script>
+import DefaultBtn from "@/components/DefaultBtn.vue";
 export default {
-  setup() {
-    return {};
+  components: {
+    DefaultBtn,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+      default: "please add a Title",
+    },
+    btnText: {
+      type: String,
+      required: true,
+      default: "See more",
+    },
+    imgSrc: {
+      type: String,
+      required: true,
+    },
   },
 };
 </script>
-
-<style lang="scss" scoped></style>
+<style scoped>
+article {
+  width: 80vmax;
+  margin: 5rem auto;
+  display: flex;
+  gap: 2rem;
+}
+section {
+  width: 70%;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+}
+img {
+  border-radius: 2rem;
+}
+h2 {
+  color: var(--color-buttons-primary);
+}
+</style>
