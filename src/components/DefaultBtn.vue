a9ab048853c7707f0c6455bee30c04d9fa2b1d57
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-06 17:34:32 +0200

    style: button styling, hover fx, :active fx

--- src/components/DefaultBtn.vue
+++ src/components/DefaultBtn.vue
@@ -14,5 +14,31 @@ export default {
 </script>
 
 <style scoped>
-@import "@/assets/btnStyle.css";
+.btn {
+  color: var(--color-accent-blue-100);
+  width: 150px;
+  border-radius: 2rem;
+  padding: 0.5rem;
+  border: 1px solid var(--color-accent-blue-80);
+  background: none;
+  cursor: pointer;
+  font-family: sans-serif;
+}
+.btn:hover {
+  outline: 2px solid var(--color-accent-blue-100);
+}
+
+.btn:active {
+  transform: scale(0.9);
+}
+
+.btn--primary {
+  background: var(--color-buttons-primary);
+  color: var(--color-secondary);
+}
+
+.btn--secondary {
+  background: var(--color-buttons-secondary);
+  color: var(--color-text);
+}
 </style>
