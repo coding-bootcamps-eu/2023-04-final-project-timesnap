d560a2707626f4efc3a53ab64fc7c2c09e07e54a
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-06 23:20:54 +0200

    initial commit for component Article Large

---
+++ src/components/ArticleLarge.vue
@@ -0,0 +1,15 @@
+<!-- Contains title, text block, image and if necessary a button -->
+
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  setup() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
