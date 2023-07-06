d0a5f53ca73414d0e86011dffe42991be026feda
Julia Stephan <julia-stephan@hotmail.de>
2023-07-06 18:24:27 +0200

    feat: adds navigation segment

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
+  <Navigation />
   <router-view />
 </template>
+
+<script>
+import Navigation from "@/components/Navigation.vue";
+
+export default {
+  components: {
+    Navigation,
+  },
+};
+</script>
---
+++ src/views/HowToUseView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="how-to-use">
+    <h1>How to use page</h1>
+  </div>
+</template>
