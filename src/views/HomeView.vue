dcc2dcb882f02f2e12a7ed8862d39a5a18891c8e
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-07 00:51:38 +0200

    adds sample elements

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,4 +2,38 @@
   <div class="home">
     <h1>Home</h1>
   </div>
+  <article-large
+    title="Set timestamps that matter"
+    imgSrc="https://picsum.photos/300/200"
+  >
+    <p>
+      Welcome to TimeSnap. This platform allows you to set your individual
+      timestamps on any video. This allows you to jump right to the content that
+      matters to you. As a community based platform you can recommend your own
+      timestamps to the public page.
+    </p>
+  </article-large>
+  <article-large
+    title="How to get started"
+    imgSrc="https://picsum.photos/300/200?3=1"
+    btnText="Find out more"
+  >
+    <p>
+      You don’t need an account to watch the videos provided by the community.
+      Simply browse through the list of already edited videos.
+      <br /><br /><span>Looking for a specific topic?</span> <br />Simply use
+      the predefined filters to find the category that you are looking for or
+      search for buzzwords to get to the topic that matters to you the most.
+    </p>
+  </article-large>
 </template>
+<style scoped>
+p {
+  font-weight: 200;
+  color: var(--color-buttons-primary);
+  line-height: 1.5;
+}
+span {
+  font-weight: 400;
+}
+</style>
