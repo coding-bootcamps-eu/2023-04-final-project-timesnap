f5be6d0aedeae6ec7bacea6da9482d1292ff006a
Julia Stephan <julia-stephan@hotmail.de>
2023-07-10 17:26:30 +0200

    feat: adds video.js player and video component. videos from youtube, google drive and vjs work with the player

--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -2,4 +2,37 @@
   <div class="videos">
     <h1>Video Overview</h1>
   </div>
+  <div v-for="{ videoUrl, id } in video" :key="id">
+    <VideoComponent :videoUrl="videoUrl" :videoType="typeSwitch(videoUrl)" />
+  </div>
 </template>
+
+<script>
+import VideoComponent from "@/components/VideoComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    VideoComponent,
+  },
+  data() {
+    return {
+      video: [],
+    };
+  },
+  methods: {
+    typeSwitch(value) {
+      if (value.includes("youtube")) {
+        return "video/youtube";
+      } else {
+        return "video/mp4";
+      }
+    },
+  },
+  async mounted() {
+    const response = await fetch("http://localhost:3333/videos");
+    const data = await response.json();
+    this.video = data;
+  },
+};
+</script>
