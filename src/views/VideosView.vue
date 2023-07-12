98d64e93bfca9799e9d07671a1d2c827aa724c02
Noah Wolters <130577232+NoahWolters@users.noreply.github.com>
2023-07-12 10:51:19 +0200

    feat: newest videos on homeView + styling

--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -2,8 +2,17 @@
   <div class="videos">
     <h1>Video Overview</h1>
   </div>
-  <div v-for="{ videoUrl, id } in video" :key="id">
+  <div
+    class="video-list"
+    v-for="{ videoUrl, id, title, tag, createdAt } in video"
+    :key="id"
+  >
     <VideoComponent :videoUrl="videoUrl" :videoType="typeSwitch(videoUrl)" />
+    <div class="h3">
+      <h3>Title: {{ title }}</h3>
+      <h3>Tags: {{ tag }}</h3>
+      <h3>Created: {{ createdAt }}</h3>
+    </div>
   </div>
 </template>
 
@@ -36,3 +45,19 @@ export default {
   },
 };
 </script>
+
+<style>
+.videos {
+  font-weight: 200;
+  color: var(--color-buttons-primary);
+  line-height: 1.5;
+}
+.video-list {
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 1em;
+  width: 100em;
+  margin-bottom: 5em;
+}
+</style>
