066cd6b9d6c7bf5526f4929d93585b4e5f1ab848
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-12 10:09:53 +0200

    feat: VideoDetailseite hinzugefügt

---
+++ src/views/VideoDetail.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="videos">
+    <h1>Video Overview</h1>
+  </div>
+  <div v-for="{ videoUrl, id } in video" :key="id">
+    <VideoComponent :videoUrl="videoUrl" :videoType="typeSwitch(videoUrl)" />
+  </div>
+</template>
+
+<script>
+import VideoComponent from "@/components/VideoComponent.vue";
+
+export default {
+  name: "HomeView",
+  components: {
+    VideoComponent,
+  },
+  data() {
+    return {
+      video: [],
+    };
+  },
+  methods: {
+    typeSwitch(value) {
+      if (value.includes("youtube")) {
+        return "video/youtube";
+      } else {
+        return "video/mp4";
+      }
+    },
+  },
+  async mounted() {
+    const response = await fetch("http://localhost:3333/videos");
+    const data = await response.json();
+    this.video = data.filter((element) => element.id === "9a0828f5");
+  },
+};
+</script>
