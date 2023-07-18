0e9c8ae2e312c2404d020cf363f3e14a56a061f1
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-18 10:36:51 +0200

    refactor: adds redirect to addNewVideo page on video overview page

--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -1,6 +1,7 @@
 <template>
   <main>
     <h1>Video Overview</h1>
+    <default-btn btnText="add new video" @click="openAddVideoPage" />
     <section class="video-preview" v-for="video in videos" :key="video.id">
       <VideoBlock :videoData="video" @videoDataId="videoDetailPage" />
     </section>
@@ -10,10 +11,13 @@
 <script>
 import VideoBlock from "@/components/VideoBlock.vue";
 
+import DefaultBtn from "@/components/DefaultBtn.vue";
+
 export default {
   name: "VideoView",
   components: {
     VideoBlock,
+    DefaultBtn,
   },
   data() {
     return {
@@ -34,6 +38,9 @@ export default {
     },
     videoDetailPage(id) {
       this.$router.push(`/videos/${id}`);
+    },
+    openAddVideoPage() {
+      this.$router.push(`/add-new-video`);
     },
   },
   async mounted() {
