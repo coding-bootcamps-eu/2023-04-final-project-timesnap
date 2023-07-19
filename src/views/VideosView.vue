65f943df07a4f3d4225475eb1c6b6493fa1a8d98
Noah Wolters <130577232+NoahWolters@users.noreply.github.com>
2023-07-19 11:43:17 +0200

    feat: filterfunktion für videos

--- src/views/VideosView.vue
+++ src/views/VideosView.vue
@@ -1,7 +1,27 @@
 <template>
   <main>
     <h1>Video Overview</h1>
-    <section class="video-preview" v-for="video in videos" :key="video.id">
+    <div class="filter-container">
+      <label for="mainTopic">Main Topic:</label>
+      <select id="mainTopic" v-model="selectedMainTopic" @change="applyFilters">
+        <option value="">Alle</option>
+        <option v-for="group in groups" :value="group.id" :key="group.id">
+          {{ group.title }}
+        </option>
+      </select>
+      <label for="tags">Tags:</label>
+      <select id="tags" v-model="selectedTag" @change="applyFilters">
+        <option value="">Alle</option>
+        <option v-for="tag in keyTags" :value="tag.id" :key="tag.id">
+          {{ tag.tag }}
+        </option>
+      </select>
+    </div>
+    <section
+      class="video-preview"
+      v-for="video in filteredVideos"
+      :key="video.id"
+    >
       <VideoBlock :videoData="video" @videoDataId="videoDetailPage" />
     </section>
   </main>
@@ -18,6 +38,11 @@ export default {
   data() {
     return {
       videos: [],
+      filteredVideos: [],
+      selectedMainTopic: "",
+      selectedTag: "",
+      groups: [],
+      keyTags: [],
     };
   },
   methods: {
@@ -35,11 +60,39 @@ export default {
     videoDetailPage(id) {
       this.$router.push(`/videos/${id}`);
     },
+    applyFilters() {
+      this.filteredVideos = this.videos.filter((video) => {
+        const groupIds = video.groupId;
+        const tagIds = video.keyTagId;
+        const mainTopicFilter =
+          this.selectedMainTopic === "" ||
+          groupIds.includes(this.selectedMainTopic) ||
+          video.id === this.selectedMainTopic;
+
+        const tagFilter =
+          this.selectedTag === "" || tagIds.includes(this.selectedTag);
+
+        return mainTopicFilter && tagFilter;
+      });
+    },
   },
   async mounted() {
-    const response = await fetch("http://localhost:3333/videos");
-    const data = await response.json();
-    this.videos = data;
+    try {
+      const responseVideos = await fetch("http://localhost:3333/videos");
+      const responsekeyTags = await fetch("http://localhost:3333/keyTags");
+      const responseGroups = await fetch("http://localhost:3333/groups");
+
+      const dataVideos = await responseVideos.json();
+      const dataKeyTags = await responsekeyTags.json();
+      const dataGroups = await responseGroups.json();
+
+      this.videos = dataVideos;
+      this.filteredVideos = dataVideos;
+      this.keyTags = dataKeyTags;
+      this.groups = dataGroups;
+    } catch (error) {
+      console.error("Fehler beim Abrufen der Videos:", error);
+    }
   },
 };
 </script>
