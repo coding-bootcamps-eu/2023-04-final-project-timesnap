7c75ec83eec62b612c4d6875a0623d5c1d067872
Jimmy Ng <jimmy_ng@outlook.de>
2023-07-21 01:06:45 +0200

    adds redirect

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -19,6 +19,7 @@
       title="How to get started"
       imgSrc="https://picsum.photos/300/200?3=1"
       btnText="How to use"
+      @triggerRedirect="handleRedirect()"
     >
       <p>
         You don’t need an account to watch the videos provided by the community.
@@ -63,6 +64,9 @@ export default {
     return { searchVideos };
   },
   methods: {
+    handleRedirect() {
+      this.$router.push(`/faq`);
+    },
     typeSwitch(value) {
       if (value.includes("youtube")) {
         return "video/youtube";
