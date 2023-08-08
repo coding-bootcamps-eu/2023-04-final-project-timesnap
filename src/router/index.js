961ed126ba0f5a750674d9c80b8538143b105259
Jimmy Ng <jimmy_ng@outlook.de>
2023-08-08 12:06:18 +0200

    jumps to top of page on redirect

--- src/router/index.js
+++ src/router/index.js
@@ -69,6 +69,9 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes,
+  scrollBehavior() {
+    return { top: 0 };
+  },
 });
 router.beforeEach(() => {
   const searchVideos = useSearchStore();
