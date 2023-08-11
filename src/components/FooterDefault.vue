0747ca98d83b86cb9d050e968894775f5a1513ef
Jimmy Ng <jimmy_ng@outlook.de>
2023-08-11 19:42:16 +0200

    styling: responsiveness

--- src/components/FooterDefault.vue
+++ src/components/FooterDefault.vue
@@ -1,31 +1,30 @@
 <template>
   <footer class="footer">
-    <div footer-content>
-      <nav class="links">
-        <p>&copy; 2023 TimeSnap.</p>
-        <router-link :to="{ name: 'privacy' }">Privacy</router-link>
-        <router-link :to="{ name: 'contact' }">Contact</router-link>
+    <p>&copy; 2023 TimeSnap</p>
+    <nav class="links">
+      <router-link :to="{ name: 'privacy' }">Privacy</router-link>
+      <router-link :to="{ name: 'contact' }">Contact</router-link>
 
-        <router-link class="nav-link" :to="{ name: 'about' }"
-          >About</router-link
-        >
-      </nav>
-    </div>
+      <router-link class="nav-link" :to="{ name: 'about' }">About</router-link>
+    </nav>
   </footer>
 </template>
 <style scoped>
 .footer {
-  padding: 20px;
+  display: flex;
+  flex-direction: row;
+  margin-inline: 2em;
+  margin-block: 1em;
+  column-gap: 2em;
+  row-gap: 0.5rem;
+  flex-wrap: wrap;
 }
 
 .links {
   display: flex;
-  gap: 2em;
-  padding: 1em 2em;
+  column-gap: 2em;
+  row-gap: 0.5rem;
   align-items: center;
-}
-p {
-  width: fit-content;
-  margin-right: 2em;
+  justify-items: center;
 }
 </style>
